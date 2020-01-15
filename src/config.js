4a996d7668ec8c1b05f5b8848073c3bfc7ecf1ae
nicholasmueller <nicholasmueller91@gmail.com>
2020-01-15 12:42:29 +0700

    update config to ropsten env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  watcherUrl: 'http://localhost:7534',
-  plasmaFrameworkAddress: '0xc673e4ffcb8464faff908a6804fe0e635af0ea2f'
+  watcherUrl: 'https://dev-73f526d-watcher-info-ropsten-01.omg.network',
+  plasmaFrameworkAddress: '0x393d593cc7bdc4582a8d02a6a6a644467d533ad8'
 }
