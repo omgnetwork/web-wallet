4895daea2d374bcdc9311e9f40f03ac1a2305c82
nicholasmueller <nicholasmueller91@gmail.com>
2020-02-21 16:54:25 +0700

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/browser';
 
 import App from 'containers/app/App';
 import store from 'store';
 
 import './index.scss';
+
+Sentry.init({
+  dsn: 'https://631acdc5818b4216905974e5490515e2@sentry.io/2732241'
+});
 
 ReactDOM.render(
   <Provider store={store}>
