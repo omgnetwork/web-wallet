8adc1207ca211ec690ad4b70cac24da9f966086f
Khun Pong <k.cheecharern@gmail.com>
2020-07-20 07:26:14 +0700

    clean up

--- src/services/errorService.js
+++ src/services/errorService.js
@@ -46,12 +46,12 @@ class ErrorService {
     // omg-js create UTXOs insufficient funds error
     if (error.message && error.message.includes('Insufficient funds. Needs ')) {
       const tokenAddress = error.message.split(' ').find(i => i.startsWith('0x'));
-      const token = await getToken(tokenAddress);
+      const { decimals, name } = await getToken(tokenAddress);
       const tokenAmount = error.message.split(' ').find((i) => {
         return i.match(/(?!0x)\d*/)[0] !== '';
       });
-      const decimalAmount = logAmount(tokenAmount, token.decimals);
-      return 'Insufficient funds. Needs '.concat(decimalAmount.toString(), ' more of ', token.name, ' to cover payments and fees');
+      const decimalAmount = logAmount(tokenAmount, decimals);
+      return 'Insufficient funds. Needs '.concat(decimalAmount.toString(), ' more of ', name, ' to cover payments and fees');
     }
 
     // try get reason from evm error message
