4b1beaf2e3a3b8d37fc15cd729f8886052a03e92
nicholasmueller <nicholasmueller91@gmail.com>
2020-11-05 16:43:07 +0700

    implement nicer design

--- src/containers/modals/ledger/LedgerConnect.js
+++ src/containers/modals/ledger/LedgerConnect.js
@@ -19,7 +19,13 @@ import { closeModal, ledgerConnect } from 'actions/uiAction';
 
 import Button from 'components/button/Button';
 import Modal from 'components/modal/Modal';
+
 import ledger from 'images/ledger_connect.png';
+import boxarrow from 'images/boxarrow.svg';
+import eth from 'images/eth.svg';
+import key from 'images/key.svg';
+import lock from 'images/lock.svg';
+
 import * as styles from './LedgerConnect.module.scss';
 
 function LedgerConnect ({ submit, open }) {
@@ -41,12 +47,33 @@ function LedgerConnect ({ submit, open }) {
       </div>
 
       <div className={styles.title}>Are you connecting with Ledger?</div>
-      <div className={styles.description}>If so, please make sure Contract Data is allowed in your Ledger&apos;s Ethereum application settings.</div>
+      <div className={styles.description}>If so, please check the following steps to make sure Contract Data is allowed in your Ledger&apos;s Ethereum application settings.</div>
+
       <div className={styles.steps}>
-        <p>1. Connect and unlock your Ledger device.</p>
-        <p>2. Open the Ethereum application.</p>
-        <p>3. Press the right button to navigate to Settings. Then press both buttons to validate.</p>
-        <p>4. In the Contract data settings, press both buttons to allow contract data in transactions. The device displays Allowed.</p>
+        <div className={styles.step}>
+          <div className={styles.iconWrapper}>
+            <img src={lock} alt='lock' />
+          </div>
+          <div className={styles.text}>1. Connect and unlock your Ledger device.</div>
+        </div>
+        <div className={styles.step}>
+          <div className={styles.iconWrapper}>
+            <img src={eth} alt='eth' />
+          </div>
+          <div className={styles.text}>2. Open the Ethereum application.</div>
+        </div>
+        <div className={styles.step}>
+          <div className={styles.iconWrapper}>
+            <img src={boxarrow} alt='boxarrow' />
+          </div>
+          <div className={styles.text}>3. Press the right button to navigate to Settings. Then press both buttons to validate.</div>
+        </div>
+        <div className={styles.step}>
+          <div className={styles.iconWrapper}>
+            <img src={key} alt='key' />
+          </div>
+          <div className={styles.text}>4. In the Contract data settings, press both buttons to allow contract data in transactions. The device displays Allowed.</div>
+        </div>
       </div>
 
       <div className={styles.buttons}>
